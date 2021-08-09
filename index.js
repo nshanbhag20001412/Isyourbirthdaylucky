80a083abfe2b8abee1c4de68ebece78685204f14
nshanbhag20001412 <nshanbhag1412@gmail.com>
2021-08-09 19:06:02 +0530

    completed

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
 const birthDate =document.querySelector("#date");
 const number =document.querySelector("#number");
-const btn =document.querySelector("#butt");
+const checkBtn =document.querySelector("#checkBtn");
+
+const dsiplay=document.querySelector("#display")
+
+
+function prepareNumber(){
+    var date=birthDate.value;
+    date=date.replaceAll("-","0");
+
+    let sum=0;
+
+    for( let i=0;i<date.length;i++){
+        
+        sum=sum +Number(date[i]);
+    }
+   
+    checkIfLucky(sum);
+}
+
+function checkIfLucky(sum){
+    
+    let luckyNumber=number.value;
+    if(sum%luckyNumber===0){
+      
+        dsiplay.innerHTML="Woohooo!   Your Birthday is Lucky!!!!";
+    }
+    else{
+
+        dsiplay.innerHTML="Birthday is not lucky";
+    }
+}
+
+checkBtn.addEventListener("click", prepareNumber);
 
